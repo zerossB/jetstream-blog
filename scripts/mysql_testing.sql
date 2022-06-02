d13ae92a6bdb977e2b6376cc67c377796ff46114
Guilherme Haynes Howe <zerossb@gmail.com>
2022-06-01 21:32:27 -0300

    Update Dockerfile

--- scripts/mysql_testing.sql
+++ scripts/mysql_testing.sql
@@ -1,7 +1,7 @@
 CREATE
-DATABASE blog_testing;
+DATABASE blog_server_testing;
 
 CREATE
-USER blog_testing@'%' IDENTIFIED WITH mysql_native_password BY 'blog_testing';
+USER blog_server_testing@'%' IDENTIFIED WITH mysql_native_password BY 'blog_server_testing';
 
-GRANT ALL PRIVILEGES ON blog_testing.* TO blog_testing@'%';
+GRANT ALL PRIVILEGES ON blog_server_testing.* TO blog_server_testing@'%';
